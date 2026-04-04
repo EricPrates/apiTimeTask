f54af5eb763c9b7bef29cce0d7df6b879bb81205
EricMarasco <eric.marasco@viannasempre.com.br>
2026-04-04 00:00:51 -0300

    correção de rota dinamica

--- src/routes/config.routes.ts
+++ src/routes/config.routes.ts
@@ -1,6 +1,6 @@
 import { NextFunction, Request, Response } from "express";
 import { adminProcessToken, processToken } from "../auth/middleware";
-import { AuthRequest } from "../types/types";
+
 const adminRoutes = {
     DELETE: ['/users', '/tasks', '/users/:id'],
     GET: ['/users', '/tasks'],
@@ -11,9 +11,11 @@ const adminRoutes = {
 export async function dinamicRoutes(req :Request, res: Response, next: NextFunction) {
     const method = req.method;
     const arrayUrl = req.path.split('/') 
-    const endpointing = arrayUrl[2];
+    const path = arrayUrl.slice(2).join(('/'));
 
-    if(method in adminRoutes && adminRoutes[method as keyof typeof adminRoutes].some(endpoint => endpoint === `/${endpointing}`)){
+    if(method in adminRoutes && adminRoutes[method as keyof typeof adminRoutes].some(endpoint => {
+    const pattern = endpoint.replace(/:[^/]+/g, '[^/]+');
+    return new RegExp(`^${pattern}$`).test(`/${path}`)})){
         return adminProcessToken(req, res, next);
     }
     next();
